2f4324db96779baaf2ad61b04407d454a0ccc575
Antonella Colon <antonella.colon@upr.edu>
2021-04-02 19:13:29 -0400

    a

---
+++ responsive/random.js
@@ -0,0 +1,8 @@
+console.log("working");
+let side = document.getElementById("grab").addEventListener("click", appearSide)
+let sidebar = document.getElementById("whole");
+
+function appearSide(){
+sidebar.classList.toggle
+("hidden")
+}
