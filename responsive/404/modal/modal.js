78442a8a7bbafae104ea4718ce529b819ff295e9
Antonella Colon <antonella.colon@upr.edu>
2021-04-04 09:05:02 -0400

    modal

---
+++ responsive/404/modal/modal.js
@@ -0,0 +1,19 @@
+let btn = document.getElementById("btn").addEventListener("click",runny)
+let modalText = document.querySelector("h1")
+let grab = document.getElementById("main")
+let get = document.getElementById("otherModal")
+
+let icon = document.getElementById("ii").addEventListener("click",ran)
+
+
+function runny(){
+
+ grab.classList.toggle("hidden");
+ get.classList.toggle("hidden");
+
+}
+
+function ran(){
+    grab.classList.toggle("hidden");
+    get.classList.toggle("hidden");
+}
