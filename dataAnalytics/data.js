6317714d96b81ef326289e6ea843ee87ec5c36a3
Antonella Colon <antonella.colon@upr.edu>
2021-05-08 07:32:42 -0400

    removed

---
+++ dataAnalytics/data.js
@@ -0,0 +1,8 @@
+const hiding = document.getElementById("hide");
+const btn = document.getElementById("bot");
+bot.addEventListener("click", showw);
+console.log("hello");
+
+function showw() {
+  hiding.style.color = "green";
+}
--- week3/idk/storr.js
+++
@@ -1,3 +0,0 @@
-localStorage.setItem("nombre","Juanito")
-localStorage.setItem("edad","25")
-console.log(localStorage.getItem('edad'))
